d9508450edf0ac1a4824ad71c830911ffe11ed6f
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-03-05 10:14:38 -0300

    corrigindo expressao regular

--- src/app/pipes/format-cidade-name.pipe.ts
+++ src/app/pipes/format-cidade-name.pipe.ts
@@ -5,14 +5,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FormatCidadeNamePipe implements PipeTransform {
 
-  transform(value: string | undefined,): string | undefined {
+  transform(value: string | undefined): string | undefined {
     let words = value?.split(" ");
-    let pattern = /(dos|da|do|das)/g
+    let pattern = /^(dos|da|do)$/g
 
     return words?.map((word) => {
-      if (!word.match(pattern))
-        return word[0].toUpperCase() + word.substring(1);
-      return word
+      if (word.match(pattern))
+        return word
+      return word[0].toUpperCase() + word.substring(1);
     })
       .join(" ")
   }
